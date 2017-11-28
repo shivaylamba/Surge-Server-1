1cbdc782827ec1270f351d96ce1f524cc2b5a6f9
Prince Sky <33918448+Surge-Master@users.noreply.github.com>
2017-11-28 09:08:17 +0530

    Update symbolcolor.js

--- wavelength-plugins/symbolcolor.js
+++ wavelength-plugins/symbolcolor.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const fs = require('fs');
-let selectors:
+let selectors;
 
 function writeSymbolCSS() {
 	fs.appendFile('config/custom.css', selectors);
@@ -13,7 +13,10 @@ exports.commands = {
 		let args = target.split(',');
 		if (args.length < 3) return this.parse('/help symbolcolor');
 		let username = toId(args.shift());
-		let color = 'color:' + args.shift().trim() + ';'
+		let color = 'color:' + args.shift().trim() + '-userlist-user-' + username + ' em.group';
+		args.ForEach(function (room) {
+			selectors += ', #' + toId(room) + '-userlist-user-' + username + ' em.group';
+		});
 		selectors += '{ \n' + ' ' + color + '\n }';
 		Rooms('staff').add('|raw|' + WL.nameColor(target[0], true) + ' has been given symbol color by ' + WL.nameColor(user.name, true) +'.').update();
 		this.sendReply('You have given ' + WL.nameColor(target[0], true) + ' custom symbol color.');
