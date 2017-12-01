698831447929852299c1683207d42db458f07272
Prince Sky <33918448+Surge-Master@users.noreply.github.com>
2017-12-01 13:39:12 +0000

    Create pets.js

---
+++ wavelength-plugins/pets.js
@@ -0,0 +1,9 @@
+'use strict';
+/********************
+* Pets * Skrub's Pet System: Credit to wgc :3
+********************/
+
+const uuid = require('uuid');
+const pets = require('../wavelength-plugins/pets-data.js');
+let color = require('../config/color');
+let rankLadder = require('../rank-ladder');
